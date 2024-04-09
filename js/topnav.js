7f83116357b6f2ecbe7977395cbae0e12c12479a
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-04-09 10:14:31 +0100

    Initial commit

---
+++ js/topnav.js
@@ -0,0 +1,13 @@
+/**
+ * Topnav.js
+ * This javascript file is used to make the topnav responsive 
+*/
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
